fe6d97e202d23dd7e932902a6bee124c1e0f01f1
pzb568 <pengzb886@gmail.com>
2019-08-25 22:25:30 +0800

    02添加变量声明.go

--- 02变量声明.go
+++ 02变量声明.go
@@ -6,16 +6,12 @@ import (
 
 // 变量声明
 
-// 第一种方法
-/*
 var a = 12
 var b = 25
 var c = 33
-*/
-// 第二种方法
-var a, b, c = 5, 8, 9
 
 func main() {
 
 	fmt.Println("a加c除以b乘以e等于", a+c/b)
+	fmt.Println(a, b, c)
 }
